67f85a80c8864c95b0a71dcbbb504344538279da
Oliver Cook <olliewatford@gmail.com>
2024-02-07 17:15:40 +0000

    fixed build issue

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main className="min-h-screen flex flex-col items-center">
       <h1 className="text-6xl font-bold mt-4">Countries Visited 🌍</h1>
-      <p className="mt-4">Mark the countries you've visited by clicking on them.</p>
+      <p className="mt-4">Mark the countries you&apos;ve visited by clicking on them.</p>
       <p>Scroll to zoom. Hover to see country name.</p>
       <div className="h-[80vh]">
         <Map />
